ee50083cdba16bf231aecc443a7ec0c029cbe7a6
tomvieilledent <tomvieilledent@gmail.com>
2025-10-20 13:52:41 +0200

    Fix: typo

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %u bytes\n", sizeof(char));
-	printf("Size of int: %u bytes\n", sizeof(int));
-	printf("Size of long int: %u bytes\n", sizeof(long int));
-	printf("Size of long long int: %u bytes\n", sizeof(long long int));
-	printf("Size of float: %u bytes\n", sizeof(float));
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
