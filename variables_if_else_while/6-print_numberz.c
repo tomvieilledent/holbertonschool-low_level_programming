c50f7e84ba2c1c0ff06a62ac4d94e1ce35e53953
tomvieilledent <tomvieilledent@gmail.com>
2025-10-21 15:13:18 +0200

    Feat: Create a program that prints a single digit number from 0 to 9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char digit;
+
+	for (digit = 48; digit <= 57; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+return (0);
+}
