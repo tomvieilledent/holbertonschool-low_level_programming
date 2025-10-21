583c52f9798e5dd1c7e7031619d6681ee6cc26cc
tomvieilledent <tomvieilledent@gmail.com>
2025-10-21 15:45:41 +0200

    Feat: create a program that prints hexadecimal from 0 to e

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char asciiLetter;
+
+	for (asciiLetter = 48; asciiLetter <= 57; asciiLetter++)
+{
+	putchar(asciiLetter);
+}
+	asciiLetter = 64;
+
+	for (asciiLetter = 97; asciiLetter <= 101; asciiLetter++)
+{
+	putchar(asciiLetter);
+}
+putchar('\n');
+return (0);
+}
