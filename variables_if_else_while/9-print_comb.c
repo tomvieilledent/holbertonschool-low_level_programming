2f6c05a5e57b1c19ebd64ecea293cfdadaea5544
tomvieilledent <tomvieilledent@gmail.com>
2025-10-21 17:13:16 +0200

    Feat: create a program that prints 0 to 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,12 +13,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n <= 56; n++)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
+		if (n < 57)
+		{
 		putchar(44);
 		putchar(32);
+		}
 	}
-	putchar(57);
+	putchar('\n');
 return (0);
 }
