44e5c45b3166bfe60c9c01f5d088dbb77e9fb580
tomvieilledent <tomvieilledent@gmail.com>
2025-10-22 13:14:32 +0200

    feat: crate a program that prints alphabet x 10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+
+	for(n = 1; n <= 10; n++)
+	{
+	char asciiLetter;
+
+	asciiLetter = 97;
+
+	while (asciiLetter < 123)
+	{
+		_putchar(asciiLetter);
+		asciiLetter = asciiLetter + 1;
+	}
+		_putchar('\n');
+}
+
+}
