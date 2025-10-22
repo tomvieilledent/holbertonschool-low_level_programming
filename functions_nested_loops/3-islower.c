b1e18c7bd348a24dcb38ace6ac0a7da6f2ce0c78
tomvieilledent <tomvieilledent@gmail.com>
2025-10-22 16:04:44 +0200

    feat: create a program the checks if variable is lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+
+/**
+ * _islower - Checks if a character is a lowercase letter
+ * @c: The character to check (ASCII value)
+ *
+ * Return: 1 if c is a lowercase letter, 0 otherwise
+ */
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
