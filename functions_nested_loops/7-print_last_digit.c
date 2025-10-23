9db88bdcf29e30937902bf38e6bd2da285dda5be
tomvieilledent <tomvieilledent@gmail.com>
2025-10-23 14:39:11 +0200

    fix

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -15,12 +15,12 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 		{
-		_putchar('0' + -n % 10);
-		return ('0' + -n % 10);
+		_putchar(-n % 10 + '0');
+		return (-n % 10);
 		}
 	else
 		{
-		_putchar('0' + n % 10);
-		return ('0' + n % 10);
+		_putchar(n % 10 + '0');
+		return (n % 10);
 		}
 }
