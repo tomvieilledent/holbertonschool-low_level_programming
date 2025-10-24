b656f7ca105e2d578e7ad137c3884e4fbcc8a002
tomvieilledent <tomvieilledent@gmail.com>
2025-10-24 14:40:46 +0200

    fix: update documentation for print_to_98 function

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,21 +2,22 @@
 #include "stdio.h"
 
 /**
- * add - returns the sum of two integers
- * @a: The first integer to be added.
- * @b: The second integer to be added.
- *
- * Return: The sum of the two integers.
- */
+* print_to_98 - prints all natural numbers from n to 98,
+* followed by a new line
+* @n: the starting number
+*
+* Description: prints all numbers from n to 98, separated by a comma
+* and a space, in ascending or descending order depending on n.
+*/
 
 void print_to_98(int n)
 
 {
-int i;
-for (i = n; i < 98; i++)
-	printf("%d, ", i);
-for (i = n; i > 98; i--)
-	printf("%d, ", i);
-printf("98\n");
-}
+	int i;
 
+	for (i = n; i < 98; i++)
+		printf("%d, ", i);
+	for (i = n; i > 98; i--)
+		printf("%d, ", i);
+	printf("98\n");
+}
