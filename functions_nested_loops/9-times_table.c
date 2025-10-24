69553c7bba88709e575e93eeac093169e403c62f
tomvieilledent <tomvieilledent@gmail.com>
2025-10-24 13:00:56 +0200

    fix: correct formatting and indentation in times_table function

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * times_table - prints every minute from 00:00 to 23:59
@@ -13,34 +14,34 @@ void times_table(void)
 		for (n2 = 0; n2 <= 9; n2++)
 		{
 			if (n2 == 0)
-				{
+			{
 				t = n1 * n2;
-					if (t > 10)
-					{
-						_putchar(t / 10 + '0');
-						_putchar(t % 10 + '0');
-					}
-					if (t <= 10)
+				if (t > 10)
+				{
+					_putchar(t / 10 + '0');
+					_putchar(t % 10 + '0');
+				}
+				if (t <= 10)
 				{
 					_putchar(t / 10 + '0');
 				}
-				}
+			}
 			else
-				{
+			{
 				t = n1 * n2;
 				_putchar(',');
 				_putchar(' ');
-					if (t > 10)
-					{
-						_putchar(t / 10 + '0');
-						_putchar(t % 10 + '0');
-					}
-					if (t <= 10)
+				if (t > 10)
+				{
+					_putchar(t / 10 + '0');
+					_putchar(t % 10 + '0');
+				}
+				if (t <= 10)
 				{
 					_putchar(' ');
-					_putchar(t / 10 + '0');
+					_putchar(t % 10 + '0');
 				}
-				}
+			}
 		}
 		_putchar('\n');
 	}
