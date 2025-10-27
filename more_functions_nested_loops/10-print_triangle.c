27adf47037971204304c7adea854a7a55145ea2f
tomvieilledent <tomvieilledent@gmail.com>
2025-10-27 15:31:53 +0100

    feat: implement function to draw a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a right-aligned triangle using the character '#'
+ * @size: The size (height) of the triangle
+ */
+
+void print_triangle(int size)
+
+{
+	int n, a, b;
+
+	if (size > 0)
+	{
+		for (n = 1; n <= size; n++)
+		{
+			for (a = size - n; a > 0; a--)
+				_putchar('.');
+			for (b = 1; b <= n; b++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
