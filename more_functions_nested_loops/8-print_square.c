216948f2335357b435173b133a3369290a674491
tomvieilledent <tomvieilledent@gmail.com>
2025-10-27 12:42:41 +0100

    feat: create a program that draw a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square : draw a square
+ * @size: The size of square
+ * @l: The number of lines
+ * @c: The number of columns
+ */
+
+void print_square(int size)
+
+{
+	int l, c;
+
+	for (l = 0; l <= size; l++)
+	{
+		for (c = 0; c <= size; c++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
