c88e555314ad4dccaf1a66f2f09f6fb1abbbba1d
tomvieilledent <tomvieilledent@gmail.com>
2025-10-27 09:13:58 +0100

    feat: implement mul function to multiply two integers and update main.h

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - Multiplies two integers
+ * Description: Contains a function `mul` that multiplies two integers.
+ *
+ * @a: The first integer to multiply
+ * Return: The product of `a` and `b`
+ */
+int mul(int a, int b)
+
+{
+return(a * b);
+}
