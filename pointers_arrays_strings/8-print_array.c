ccffb8f109aa8239a0e017ff9eb04d62e903f8ed
tomvieilledent <tomvieilledent@gmail.com>
2025-10-29 13:38:44 +0100

    Add print_array function

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an integer array.
+ * @a: Pointer to the first element of the array.
+ * @n: Number of elements of the array to be printed.
+ *
+ * Description: This function prints the first n elements of
+ * an integer array, separated by a comma and a space.
+ * It then prints a new line at the end.
+ *
+ * Return: Nothing.
+ */
+
+void print_array(int *a, int n)
+
+{
+	int c;
+for (c = 0; c < n; c++)
+{
+	if (c == n - 1)
+	{
+	
+	}
+	printf("%d", a[c]);
+}
+}
