ec9490e0f1e58bac9fd31d81a0380c8b2d22a8af
tomvieilledent <tomvieilledent@gmail.com>
2025-10-29 13:45:50 +0100

    fix

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -18,10 +18,14 @@ void print_array(int *a, int n)
 	int c;
 for (c = 0; c < n; c++)
 {
-	if (c == n - 1)
+	if (c < (n - 1))
 	{
-	
+		printf("%d ,", a[c]);
 	}
-	printf("%d", a[c]);
+	else
+	{
+		printf("%d", a[c]);
+	}
 }
+printf("\n");
 }
