8e0699b5888ef41da80ae175afae45cedfc4f97b
tomvieilledent <tomvieilledent@gmail.com>
2025-11-03 10:11:40 +0100

    feat: implement _memcpy function to copy memory area

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -14,5 +14,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
