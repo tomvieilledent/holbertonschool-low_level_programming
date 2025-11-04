8755f53fec3df45d35261d1799458bc4603bbd6f
tomvieilledent <tomvieilledent@gmail.com>
2025-11-04 13:37:21 +0100

    fix: update documentation for _print_rev_recursion function and remove unnecessary newline

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse, followed by a new line.
+ * _print_rev_recursion - Prints a string in reverse.
  * @s: Pointer to the string to be printed.
  * Description:
  * This function prints a string using recursion. Each character of the string
  * is printed one by one, followed by a new line at the end.
- * @return: void
  */
 
 void _print_rev_recursion(char *s)
@@ -15,8 +14,7 @@ void _print_rev_recursion(char *s)
 	if (*s == '\0')
 	{
 		return;
-		_putchar('\n');
-	}
+		return;
 	else
 	{
 		_print_rev_recursion(s + 1);
