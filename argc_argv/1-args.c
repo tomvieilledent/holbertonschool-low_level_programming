9e109c4f8979177b305de5175ae0d534933ca125
tomvieilledent <tomvieilledent@gmail.com>
2025-11-05 15:18:53 +0100

    feat: add program to count command line arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the name of the program followed by a new line.
+ * The program name is always the first argument (argv[0]).
+ * @argc: argument count
+ * @argv: array of argument strings
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char *argv[])
+
+{
+	(void)argv;
+	printf("%d\n", (argc - 1));
+	return (0);
+}
