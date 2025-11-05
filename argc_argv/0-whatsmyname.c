7b28bd8804a2614e22b31f1ca90d2551af505fee
tomvieilledent <tomvieilledent@gmail.com>
2025-11-05 15:10:20 +0100

    feat: add initial implementation of program name printer and header file

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the name of the program followed by a new line.
+ * The program name is always the first argument (argv[0]).
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char *argv[])
+
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+}
