2aafc9917f9809bab3cba4fe3213df8d0c68e9b2
tomvieilledent <tomvieilledent@gmail.com>
2025-11-10 09:43:41 +0100

    feat: implement create_array function to initialize an array of chars

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - Creates an array of chars, and init it with a specific char
+ * @size: The size of the array to create
+ * @c: The char to initialize the array with
+ *
+ * Return: Pointer to the array, or NULL if it fails or if size is 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	arr = malloc(sizeof(char) * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+	return (arr);
+}
