349a016943b4ddaf7bee40b195634c6cb4817ac9
tomvieilledent <tomvieilledent@gmail.com>
2025-11-10 09:46:03 +0100

    fix: add check for zero size in create_array function

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,6 +14,9 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	arr = malloc(sizeof(char) * size);
 	if (arr == NULL)
 		return (NULL);
