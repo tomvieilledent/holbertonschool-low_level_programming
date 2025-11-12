72078920a23af2fc030fe375510122b2cdba8120
tomvieilledent <tomvieilledent@gmail.com>
2025-11-12 13:50:28 +0100

    fix: update condition in array_range function to prevent incorrect input handling

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 {
 int *arr, i;
 
-if (min >= max)
+if (min > max)
 	return (NULL);
 
 arr = malloc(sizeof(int) * (max - min + 1));
@@ -25,6 +25,6 @@ if (arr == NULL)
 	return (NULL);
 
 for (i = min; i <= max; i++)
-	arr[i] = i;
+	arr[(i - min)] = i;
 return (arr);
 }
