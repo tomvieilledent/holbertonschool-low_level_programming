ae8151a515c8539402a8288c530bd07d245d8135
tomvieilledent <tomvieilledent@gmail.com>
2025-11-12 14:57:55 +0100

    feat: add dog structure definition to manage dog information

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure to store information about a dog
+ * @name: Name of the dog
+ * @owner: Owner of the dog
+ * @age: Age of the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif /* DOG_H */
