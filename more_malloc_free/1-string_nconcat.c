da8452d39755ee6fba5452314e72134e2b31ba99
tomvieilledent <tomvieilledent@gmail.com>
2025-11-12 12:25:43 +0100

    style: improve code formatting and comments in string_nconcat function

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,39 +18,38 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *con;
 	unsigned int len1 = 0, len2 = 0, i = 0;
-
+	/* vérifie si s1 et s2 sont NULL */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
+	/* définie les longueurs de s1 et s2 */
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
-
+	/* alloue la mémoire necessaire et vérifie que ca a marché */
 	if (len2 < n)
 		con = malloc(sizeof(char) * (len1 + len2 + 1));
 	else
 		con = malloc(sizeof(char) * (len1 + n + 1));
 	if (con == NULL)
 		return (NULL);
-
+	/* copie s1 dans con */
 	for (i = 0; i < len1; i++)
 		con[i] = s1[i];
-
+	/* en fonction de n copie s2 dans con */
 	if (len2 < n)
 	{
 		for (i = len1; i < len1 + len2; i++)
 			con[i] = s2[i - len1];
 	}
-
 	if (len2 >= n)
 	{
 		for (i = len1; i < len1 + n; i++)
 			con[i] = s2[i - len1];
 	}
-
+	/* ajoute le caractere nul */
 	con[i] = '\0';
 	return (con);
 }
