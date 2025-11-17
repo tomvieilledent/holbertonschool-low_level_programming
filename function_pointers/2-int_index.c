6264dbd61b3f077326925ea829162dea7d662d88
tomvieilledent <tomvieilledent@gmail.com>
2025-11-17 11:55:11 +0100

    feat: add int_index function to find the index of the first element that matches a condition

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * array_iterator - Executes a function given as a parameter
+ *					on each element of an array.
+ * @array: The array to iterate over.
+ * @size: The size of the array.
+ * @action: A pointer to the function to be used.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+
+{
+	int i;
+
+	if (size <= 0)
+		return(-1);
+
+	for (i = 0; i <= size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}	
+	return (0);
+}
