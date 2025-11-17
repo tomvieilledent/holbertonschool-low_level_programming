b9f42de388dca7d31cd84893a7d195dc4dad099c
tomvieilledent <tomvieilledent@gmail.com>
2025-11-17 11:09:36 +0100

    feat: add array_iterator function to iterate over an array and apply a given action

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * array_iterator - Executes a function given as a parameter
+ *					on each element of an array.
+ * @array: The array to iterate over.
+ * @size: The size of the array.
+ * @action: A pointer to the function to be used.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
