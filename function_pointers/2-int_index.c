e14713b0cd90424fb614bdc19f2c2b705d06869f
tomvieilledent <tomvieilledent@gmail.com>
2025-11-17 13:24:44 +0100

    fix: correct loop condition in int_index function and improve formatting

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 
 /**
- * array_iterator - Executes a function given as a parameter
- *					on each element of an array.
- * @array: The array to iterate over.
- * @size: The size of the array.
- * @action: A pointer to the function to be used.
+ * int_index - Searches for an integer in an array using a comparison function.
+ * @array: Pointer to the array of integers.
+ * @size: Number of elements in the array.
+ * @cmp: Pointer to the function used to compare values.
+ *
+ * Return: The index of the first element for
+ *		   which the cmp function does not return 0.
+ *         If no element matches or if size <= 0, returns -1.
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -14,13 +17,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || array == NULL|| cmp == NULL)
-		return(-1);
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
-	}	
+	}
 	return (-1);
 }
