ea55883bff62fba29b1e326df5d7e46a5e64a3b9
tomvieilledent <tomvieilledent@gmail.com>
2025-11-18 11:32:37 +0100

    feat: add print_strings function to print strings with a separator

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between the strings.
+ * @n: The number of strings to print.
+ * @...: The strings to be printed.
+ *
+ * Return: Nothing (void).
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+
+{
+unsigned int i = 0;
+char *charg;
+
+va_list args;
+va_start(args, n);
+
+while (i < n)
+{
+charg = va_arg(args, char *);
+printf("%s%s", charg, separator);
+i++;
+}
+printf("\n");
+}
