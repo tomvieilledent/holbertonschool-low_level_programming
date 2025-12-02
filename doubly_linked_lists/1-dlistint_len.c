525beb098990d9f214792f2cca7fcb3712575f4b
tomvieilledent <tomvieilledent@gmail.com>
2025-12-02 14:25:52 +0100

    fix: correct function documentation for dlistint_len

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a dlistint_t linked list.
+ * dlistint_len - Returns the number of elements in a dlistint_t linked list.
  * @h: Pointer to the head of the doubly linked list.
  *
- * Return: The number of nodes in the list.
+ * Return: Number of nodes in the list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
