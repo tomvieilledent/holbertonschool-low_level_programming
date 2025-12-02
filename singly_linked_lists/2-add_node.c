2353cb7aa1b2b801249675bbe5818c4ffe3da2bd
tomvieilledent <tomvieilledent@gmail.com>
2025-12-02 10:15:24 +0100

    fix: update documentation and formatting in add_node function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_list - Prints all the elements of a list_t linked list.
- * @h: Pointer to the head of the list_t list.
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Double pointer to the head of the list_t list.
+ * @str: String to be duplicated and stored in the new node.
  *
- * Return: The number of nodes in the list.
+ * Return: Address of the new element, or NULL if it failed.
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -14,27 +15,27 @@ list_t *add_node(list_t **head, const char *str)
 	char *str_cpy;
 	int len_cpy = 0;
 	list_t *node;
-	
-	if (str == NULL)
-		return(NULL);
 
-	str_cpy =strdup(str);
+	if (str == NULL)
+		return (NULL);
+
+	str_cpy = strdup(str);
 
 	if (str_cpy == NULL)
-		return(NULL);
+		return (NULL);
 
 	node = malloc(sizeof(list_t));
 
 	if (node == NULL)
-	{	
+	{
 		free(str_cpy);
 		str_cpy = NULL;
-		return(NULL);
+		return (NULL);
 	}
 
 	while (str_cpy[len_cpy] != '\0')
 		len_cpy++;
-		
+
 	node->str = str_cpy;
 	node->len = len_cpy;
 	node->next = *head;
