c8030cdb242709b66350d5d6ee3fe4f37f00dd4f
tomvieilledent <tomvieilledent@gmail.com>
2025-12-05 10:04:56 +0100

    fix: replace hardcoded file descriptor with STDOUT_FILENO in read_textfile function

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -35,7 +35,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	written_bytes = write(1, buffer, read_bytes);
+	written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
 	if (written_bytes == -1 || written_bytes != read_bytes)
 	{
 		free(buffer);
