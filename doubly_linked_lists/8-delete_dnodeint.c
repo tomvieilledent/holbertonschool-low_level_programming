caccbe11d8a527bd45865f543584e82b2bd36c76
tomvieilledent <tomvieilledent@gmail.com>
2025-12-05 09:55:00 +0100

    feat: implement delete_dnodeint_at_index function to delete a node at a specific index in a doubly linked list feat: implement read_textfile function to read a text file and print it to POSIX standard output feat: update main.h to include necessary headers and function prototypes

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Deletes a node at a given index in a
+ * doubly linked list.
+ * @head: Double pointer to head of list.
+ * @index: Index of element to delete (starts at 0).
+ *
+ * Return: 1 if success, -1 if failure.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	
+}
